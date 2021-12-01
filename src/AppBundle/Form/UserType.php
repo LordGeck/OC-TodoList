e8e3572ca3bcc9a17da2d77a85204ef6cb06e07b
LordGeck <lordgeck@gmail.com$>
2021-12-01 10:46:39 +0100

    Updated UserType for role assignment

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -23,6 +25,24 @@ class UserType extends AbstractType
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'label' => "Role"
+            ])
+        ;
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesAsArray) {
+                    return implode(', ', $rolesAsArray);
+                },
+                function ($rolesAsString) {
+                    return explode(', ', $rolesAsString);
+                }
+            ))
         ;
     }
 }
